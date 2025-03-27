87b2c7d09cb1fefd8e04b4c98f693b9fa8df9dca
Andreas <08obwand@rgtfo-me.it>
2025-03-27 08:26:42 +0100

    analogblinker v1

---
+++ src/analogblinker.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+
+#include "analogblinker.h"
+
+// ****************************************************************************
+// ************************** analogblinker ************************************
+// ****************************************************************************
+
+void analogblinker::init(uint8_t _pin1, uint8_t _pin2, uint16_t _blinkTime, uint8_t _polltime, bool _sync, bool _enable)
+{
+    pin1 = _pin1;
+    pin2 = _pin2;
+    blinkTime = _blinkTime;
+    polltime = _polltime;
+    sync = _sync;
+    enable = _enable;
+}
+
+void analogblinker::poll()
+{
+    if (!enable)
+    {
+        analogWrite(pin1, 0);
+        analogWrite(pin2, 0);
+        return;
+    }
+    else if (millis() - lastpoll > polltime)
+    {
+        step = 256 / (blinkTime / polltime);
+        dutycycle += step;
+        if (dutycycle >= 511)
+        {
+            dutycycle = 0;
+        }
+        uint16_t pwm = dutycycle > 255 ? 511 - dutycycle : dutycycle;
+        analogWrite(pin1, pwm);
+        if (sync)
+            analogWrite(pin2, pwm);
+        else
+            analogWrite(pin2, 255 - pwm);
+        lastpoll = millis();
+    }
+}
+void analogblinker::setblinktime(uint16_t bt)
+{
+    if (bt <= 10000 && bt > 0) // Wert zwischne 10 sek und 0
+        blinkTime = bt;
+    else
+        Serial.print(" Wrong Value ( 0-10000 ms )");
+}
